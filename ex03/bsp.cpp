f27f018ae874b1447f0ad13f57fa004a50c84c65
mkamei <mkamei@student.42tokyo.jp>
2022-04-11 17:44:31 +0900

    Edit ex03 bsp.cpp

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: mkamei <mkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 11:25:43 by mkamei            #+#    #+#             */
-/*   Updated: 2022/04/11 15:02:51 by mkamei           ###   ########.fr       */
+/*   Updated: 2022/04/11 17:43:37 by mkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed	f0, f1, f2;
 
-	f0 = CrossProduct(a - point, a - b);
-	f1 = CrossProduct(b - point, b - c);
-	f2 = CrossProduct(c - point, c - a);
+	f0 = CrossProduct(a - b, a - point);
+	f1 = CrossProduct(b - c, b - point);
+	f2 = CrossProduct(c - a, c - point);
 
 	return ((f0 > 0 && f1 > 0 && f2 > 0) || (f0 < 0 && f1 < 0 && f2 < 0));
 }
